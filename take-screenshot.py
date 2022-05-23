4ac23dea0aaf8651aebbf346be6333358a0e55a4
Ebrahim befkham <106017419+MystryBoy@users.noreply.github.com>
2022-05-24 01:02:15 +0430

    Update take-screenshot.py

--- take-screenshot.py
+++ take-screenshot.py
@@ -1,18 +1,12 @@
 #pip install pyautogui
-import pyautogui
 #pip install keyboard
-import keyboard
-#pip install time
-import time
 #pip install random
-import random as ran
-#pip install os
-import os
 
-var = f"screenshot{int(ran.uniform(1,10000))}.png"
+import pyautogui
+import keyboard
+import random as ran
+
 while True:
     if keyboard.is_pressed("Alt + s"):
-        screen_shot = pyautogui.screenshot(var)
-        print("screenshot taked")
-        time.sleep(1)
-        os.system(var)
+        screen_shot = pyautogui.screenshot(f"screenshot({int(ran.uniform(1,10000))}).png")
+        print("screenshot saved")
